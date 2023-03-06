d85013b93bf5f662b6b130a68ef3262f81f1d5f8
Renuga <122717138+Renurinni@users.noreply.github.com>
2023-03-06 10:12:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Data from "./Data";
+function App() {
+  return (
+    <>
+      <Header/>
+      <div className="App">
+        <Sidebar/>
+        <Data/>
+      </div>
+      </>
+  );
+}
+
+export default App;
